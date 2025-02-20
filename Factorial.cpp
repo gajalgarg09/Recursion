c75ef5d2fb6fff70ef1096bc50d47b5437772a74
Gajal Garg <gajalgarg09@gmail.com>
2025-02-20 15:34:59 +0530

    Update and rename r1_Factorial.cpp to Factorial.cpp

--- Factorial.cpp
+++ Factorial.cpp
@@ -4,12 +4,10 @@ using namespace std;
 int factorial(int n){
     if( n == 0)
         return 1;
-
     return n * factorial(n-1);
 }
 
 int main(){
-
     int n ;
     cout << "Enter the value of n : " << endl;
     cin >> n;
